e4c4febb98c00e90d07ff228bac31b227ae28d62
Joshua Turner <joshturner747@hotmail.com>
2023-05-12 12:20:56 +1000

    client gui complete

--- src/main/java/com/fireservice/newclientapp/PrimaryController.java
+++ src/main/java/com/fireservice/newclientapp/PrimaryController.java
@@ -1,12 +1,42 @@
 package com.fireservice.newclientapp;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    private TextField fireIdTextField;
+
+    @FXML
+    private Button getFiresButton;
+
+    @FXML
+    private TextArea reportTextArea;
+
+    @FXML
+    private Button sendTruckButton;
+
+    @FXML
+    void onGetFiresButtonClicked(ActionEvent event) {
+        // call web service api
+    }
+
+    @FXML
+    void onSendTruckButtonClicked(ActionEvent event) {
+        int fireId = Integer.parseInt(fireIdTextField.getText());
+        
+        // call api
+        
+    }
+
+    private void printFires(ArrayList fires){
+        
+        // layout and print fire details to report text area
+        
     }
 }
